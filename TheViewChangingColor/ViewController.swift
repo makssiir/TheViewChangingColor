f5d0155835deabf61d9778bb78f34ea1ba52a2e3
makssir <usikmaks3@gmail.com>
2023-05-03 16:13:51 +0200

    just finish a project!

--- TheViewChangingColor/ViewController.swift
+++ TheViewChangingColor/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var viewWhichChanging: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redColoreDouble: UILabel!
+    @IBOutlet weak var greenColorDouble: UILabel!
+    @IBOutlet weak var blueColorDouble: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        viewWhichChanging.layer.cornerRadius = 30
+        viewWhichChanging.layer.masksToBounds = false
+        
+        viewWhichChanging.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 2)
     }
 
-
+    
+    @IBAction func movingSliders() {
+        redColoreDouble.text = String(format: "%.2f", redSlider.value)
+        greenColorDouble.text = String(format: "%.2f", greenSlider.value)
+        blueColorDouble.text = String(format: "%.2f", blueSlider.value)
+        
+        viewWhichChanging.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 2)
+        
+        
+        
+    }
+    
+    
 }
 
